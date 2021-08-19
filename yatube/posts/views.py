8a224c0687599a2694f158be6ff9ef26e69927c0
Maksim <mm.zamyatin@gmail.com>
2021-08-19 17:28:17 +0300

    Разбил шаблоны, добавил namespace

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -4,8 +4,19 @@ from django.http import HttpResponse
 
 def index(request):
     template = 'posts/index.html'
-    return render(request, template)
+    text = 'Это главная страница проекта Yatube'
+    context = {
+        'text': text,
+    }
+    return render(request, template, context) 
 
 
 def group_posts(request, slug):
-    return HttpResponse(f'Группа {slug}')
+    template = 'posts/group_list.html'
+    text = 'Здесь будет информация о группах проекта Yatube'
+    context = {
+        'text': text,
+    }
+    return render(request, template, context) 
+
+# (не забыть про slug)раньше было - return HttpResponse(f'Группа {slug}')
